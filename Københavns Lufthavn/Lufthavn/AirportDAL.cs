0d258c7949bab0b1c0efefa1c6d8a93ccd49ecd0
Keviner123 <kevin@kevinfrost.dk>
2021-08-25 21:19:25 +0200

    .

---
+++ Københavns Lufthavn/Lufthavn/AirportDAL.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lufthavn
+{
+    public class AirportDAL
+    {
+        public string GetAirlines()
+        {
+            string resultstring = "";
+
+            using (AirportEntities AE = new AirportEntities())
+            {
+
+                foreach (var item in AE.Airports)
+                {
+                    resultstring += item.Name + "\n";
+
+                }
+            }
+
+            return (resultstring);
+        }
+
+        public string GetRoutes()
+        {
+            string resultstring = "";
+
+            using (AirportEntities AE = new AirportEntities())
+            {
+   
+                foreach (var item in AE.Routes)
+                {
+
+                    var person = (from p in AE.Routes
+                                  join e in AE.Airports
+                                  on p.OriginAirportID equals e.AirportID µ
+                                  where p.AirlineID == item.AirlineID
+                                  select new
+                                  {
+                                      ID = e.Name,
+
+
+                                  }).ToList();
+                    Console.WriteLine(item.OriginAirportID + " - " + person[0].ID);
+                    //resultstring += item.OriginAirportID;
+                }
+            }
+
+            return (resultstring);
+        }
+
+    }
+}
---
+++ Københavns Lufthavn/Lufthavn/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lufthavn
+{
+    class Program
+    {
+
+
+
+        static void Main(string[] args)
+        {
+            AirportDAL AD = new AirportDAL();
+
+            Console.WriteLine(AD.GetRoutes());
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
